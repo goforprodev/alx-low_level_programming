5a9ac84a5fb7ddd848f36844e94d2d554f3c1586
goforprodev <goforprodev@gmail.com>
2022-09-08 23:56:44 +0100

    tell me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-  * main - prints Programming is like building a multilingual puzzle to the console
+  * main- Prints to the screen
   * Return: returns 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like\ building a multilingual puzzle");
 	return (0);
 }
