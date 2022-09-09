d31de66e8864a394b00c1ae49801ca4fc71a0688
goforprodev <goforprodev@gmail.com>
2022-09-09 01:20:19 +0100

    a to an

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
   	float deci;
 
   	printf("Size of a char: %lu byte(s)\n", sizeof(A));
- 	printf("Size of a int: %lu byte(s)\n", sizeof(num));
+ 	printf("Size of an int: %lu byte(s)\n", sizeof(num));
  	printf("Size of a long int: %lu byte(s)\n", sizeof(long_num));
  	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll_num));
 	printf("Size of a float: %lu byte(s)\n", sizeof(deci));
