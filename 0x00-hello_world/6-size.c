a2a40c968f28f45966ed7d2620f29e8919782a77
goforprodev <goforprodev@gmail.com>
2022-09-09 01:25:43 +0100

    finaly

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
-  * main- Prints sizes of various types
+  * main- prints sizes of various types
   * Return: returns 0
-*/
+ */
 
 int main(void)
 {
 	char A;
-  	int num;
-  	long int long_num;
-  	long long int ll_num;
-  	float deci;
+	int num;
+	long int long_num;
+	long long int ll_num;
+	float deci;
 
-  	printf("Size of a char: %lu byte(s)\n", sizeof(A));
- 	printf("Size of an int: %lu byte(s)\n", sizeof(num));
- 	printf("Size of a long int: %lu byte(s)\n", sizeof(long_num));
- 	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll_num));
+	printf("Size of a char: %lu byte(s)\n", sizeof(A));
+	printf("Size of an int: %lu byte(s)\n", sizeof(num));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long_num));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll_num));
 	printf("Size of a float: %lu byte(s)\n", sizeof(deci));
- 	return (0);
+	return (0);
 }
