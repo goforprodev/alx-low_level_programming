3e26e11734fad123538d4bdcf7685ee7f5448248
goforprodev <goforprodev@gmail.com>
2022-09-25 00:05:38 +0100

    something to commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+* print_most_numbers - print numbers from 0-9 excluding 2 a* nd 4
+*
+* Return: Return void.
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 void print_numbers(void);
+void print_most_numbers(void);
 int _isupper(int c);
 int mul(int a, int b);
 int _isdigit(int c);
