48b21db6bc57d84b6d87ff151bd9703ad21f5b84
goforprodev <goforprodev@gmail.com>
2022-09-25 00:09:57 +0100

    soime

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,17 @@
-void print_numbers(void);
+#ifndef MAIN_H
+#define MAIN_H
+
+
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
 void print_most_numbers(void);
-int _isupper(int c);
+void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
+int _putchar(char c);
+int _isupper(int c);
+
+#endif /*MAIN_H*/
