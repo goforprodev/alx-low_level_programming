b1a6149f8019b80f927eb5bd93106469414676f7
goforprodev <goforprodev@gmail.com>
2022-09-24 23:56:15 +0100

    ddd

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * print_numbers - print the numbers from 0 to 9
- * Description: You can only use _putchar twice
- */
+* print_numbers - Print numbers from 0 - 9.
+*
+* Return: Return 0.
+*/
+
 void print_numbers(void)
 {
 	int i;
 
-	i = 0;
-	while (i < 10)
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i + '0');
-		i++;
+		_putchar(i);
 	}
 	_putchar('\n');
 }
