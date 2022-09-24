de3781b6cc0ab97a0127bf471242c25080d7c40a
goforprodev <goforprodev@gmail.com>
2022-09-24 17:34:34 +0100

    big ballon

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,6 @@
 void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
 void print_alphabet(void);
 int _putchar(char c);
 int _abs(int);
@@ -6,5 +8,4 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
+int add(int,int);
