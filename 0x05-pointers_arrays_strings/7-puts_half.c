eee45c0188f59ac765254ed0ae2439d2a1329456
goforprodev <goforprodev@gmail.com>
2022-09-25 02:02:56 +0100

    ddd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,36 +1,39 @@
 #include "main.h"
-#include <unistd.h>
-
 /**
-* puts_half - Prints half of a string
-*
-* @str: takes a pointer to a string as a parameter
-*
-* Return: Return always 0
+*puts_half - puts half of a string
+*@str: string
+*Return:void
 */
-
 void puts_half(char *str)
 {
-	int length_of_the_string = 0;
-	int i;
-	int n;
+int len = _strlen(str);
 
-	while (str[length_of_the_string] != '\0')
-	{
-		length_of_the_string++;
-	}
-	if (length_of_the_string % 2 == 0)
-	{
-		n = length_of_the_string / 2;
-	}
-	else
-	{
-		n = (length_of_the_string - 1) / 2;
-	}
-	for (i = n; i < length_of_the_string; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+if (len % 2 == 0)
+len = len / 2;
+
+else
+len = (len + 1) / 2;
+
+while (str[len] != '\0')
+{
+_putchar(str[len]);
+len++;
+}
+_putchar('\n');
 }
 
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
+}
