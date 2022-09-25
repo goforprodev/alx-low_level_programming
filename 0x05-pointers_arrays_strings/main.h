afaa93a63ad2f08358b0c36b80b1c7c5fa4f6a6d
goforprodev <goforprodev@gmail.com>
2022-09-25 02:12:01 +0100

    ss

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,18 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
-
-
-char *_strcpy(char *dest, char *src);
-void print_array(int *a, int n);
-void puts_half(char *str);
-void puts2(char *str);
-void rev_string(char *s);
-void print_rev(char *s);
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
-void swap_int(int *a, int *b);
+/**
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
 void reset_to_98(int *n);
-
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _strlen(char *s);
+int _atoi(char *s);
 #endif /*MAIN_H*/
